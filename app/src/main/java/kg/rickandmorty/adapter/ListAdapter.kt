e5e89cb9ebf57cad352a0beea762b29f8e3e0f24
Kamil Gierlach <48731610+Gierlaszek@users.noreply.github.com>
2022-01-11 13:13:44 +0100

    Cleaned code

--- app/src/main/java/kg/rickandmorty/adapter/ListAdapter.kt
+++ app/src/main/java/kg/rickandmorty/adapter/ListAdapter.kt
@@ -13,22 +13,32 @@ import kg.rickandmorty.databinding.ItemListBinding
 import kg.rickandmorty.ui.characterslist.ListOfCharactersDirections
 import kg.rickandmorty.model.Character
 
-class ListAdapter(private val clickListener: onCLickListener, private var favoriteList: List<Character>?) : PagingDataAdapter<Character, ListAdapter.CharacterViewHolder>(CharacterDiffCallback()){
+class ListAdapter(
+    private val clickListener: OnCLickListener,
+    private var favoriteList: List<Character>?
+) : PagingDataAdapter<Character, ListAdapter.CharacterViewHolder>(CharacterDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
         return CharacterViewHolder(
             ItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false),
-            clickListener,
-            favoriteList
+            clickListener
         )
     }
 
     override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
-        holder.bind(getItem(position)!!, position)
+        for (favorite in favoriteList!!) {
+            if (favorite.name == getItem(position)!!.name) {
+                getItem(position)!!.isFavorite = true
+            }
+        }
+        holder.bind(getItem(position)!!)
     }
 
+    fun setData(result: List<Character>) {
+        favoriteList = result
+    }
 
-    class CharacterDiffCallback : DiffUtil.ItemCallback<Character>(){
+    class CharacterDiffCallback : DiffUtil.ItemCallback<Character>() {
         override fun areItemsTheSame(oldItem: Character, newItem: Character): Boolean {
             return oldItem.name == newItem.name
         }
@@ -38,55 +48,35 @@ class ListAdapter(private val clickListener: onCLickListener, private var favori
         }
     }
 
-    fun setData(result: List<Character>){
-        favoriteList = result
-    }
-
-    class CharacterViewHolder(private val binding: ItemListBinding,
-                              private val clickListener: onCLickListener,
-                              private var favoriteList: List<Character>? ): RecyclerView.ViewHolder(binding.root){
+    class CharacterViewHolder(
+        private val binding: ItemListBinding,
+        private val clickListener: OnCLickListener,
+    ) : RecyclerView.ViewHolder(binding.root) {
 
         private val deadColor: Int = ContextCompat.getColor(binding.root.context, R.color.dead)
         private val aliveColor: Int = ContextCompat.getColor(binding.root.context, R.color.alive)
         private val favoriteAdded: Int = ContextCompat.getColor(binding.root.context, R.color.favorite)
 
-        fun bind(character: Character, position: Int){
+        fun bind(character: Character) {
             binding.name.text = character.name
             binding.status.text = character.status
             Picasso.get().load(character.image).into(binding.imageCharacter)
-            if(character.status.equals("Dead")){
-                binding.card.strokeColor = deadColor
-                binding.dataLL.setBackgroundColor(deadColor)
-            }else{
-                binding.card.strokeColor = aliveColor
-                binding.dataLL.setBackgroundColor(aliveColor)
-            }
+            binding.card.strokeColor = if(character.status == "Dead") deadColor else aliveColor
+            binding.textFrame.setBackgroundColor(if (character.status == "Dead") deadColor else aliveColor)
             binding.card.setOnClickListener {
-                val action = ListOfCharactersDirections.actionListOfCharactersToDetailsCharacter(character)
+                val action =
+                    ListOfCharactersDirections.actionListOfCharactersToDetailsCharacter(character)
                 it.findNavController().navigate(action)
             }
 
-            if(favoriteList != null){
-                for(favorite in favoriteList!!){
-                    if(favorite.name.equals(character.name)){
-                        character.isFavorite = true
-                        binding.addFavorite.setColorFilter(favoriteAdded)
-                    }
-                }
-            }
-            if(character.isFavorite == true){
-                binding.addFavorite.setColorFilter(favoriteAdded)
-            }else{
-                binding.addFavorite.setColorFilter(0)
-            }
-
+            binding.addFavorite.setColorFilter(if (character.isFavorite) favoriteAdded else 0)
             binding.addFavorite.setOnClickListener {
-                clickListener.onItemCLick(character, position)
+                clickListener.onItemCLick(character)
             }
         }
     }
 
-    interface onCLickListener{
-        fun onItemCLick(character: Character, position: Int)
+    interface OnCLickListener {
+        fun onItemCLick(character: Character)
     }
 }
